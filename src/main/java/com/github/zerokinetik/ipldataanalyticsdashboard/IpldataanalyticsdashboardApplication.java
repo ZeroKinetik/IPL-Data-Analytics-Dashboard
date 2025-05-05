d5f2784ec179eab0a87acc2a73b9e4bda40c459e
ZeroKinetik <hemansakkthivelms14@gmail.com>
2025-05-05 18:16:41 +0530

    init

---
+++ src/main/java/com/github/zerokinetik/ipldataanalyticsdashboard/IpldataanalyticsdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.github.zerokinetik.ipldataanalyticsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldataanalyticsdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpldataanalyticsdashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/zerokinetik/ipldataanalyticsdashboard/IpldataanalyticsdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zerokinetik.ipldataanalyticsdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldataanalyticsdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
